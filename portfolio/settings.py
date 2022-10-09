e04091f71b0f62268b3b15495f5f573f3e38be50
Naman82 <b520031@iiit-bh.ac.in>
2022-10-09 16:35:48 +0530

    version2 upload

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -30,9 +30,9 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
