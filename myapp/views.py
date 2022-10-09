16c0fc02e1c8dd0094478be7a79d1306d798ce5a
Naman82 <b520031@iiit-bh.ac.in>
2022-10-09 23:25:20 +0530

    Resume section updated

--- myapp/views.py
+++ myapp/views.py
@@ -11,6 +11,6 @@ def index(request):
         query=FormInfo(name=name,email=email,subject=subject,message=message)
         query.save()
 
-    resume=Resume.objects.all()[0]
+    resume=Resume.objects.all().first()
     # print(resume)
     return render(request,'index.html',{'resume':resume})
